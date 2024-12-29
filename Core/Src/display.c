b30199be71cc75d78a9f751bdefb0a1ecf5ac130
Michael Rosol <michaelrosol26@gmail.com>
2024-12-28 20:34:20 -0500

    new changes

---
+++ Core/Src/display.c
@@ -0,0 +1,69 @@
+#include "display.h"
+
+
+
+
+
+static uint8_t segmentNumber[10] = {
+        0x3f,  // 0
+        0x06,  // 1
+        0x5b,  // 2
+        0x4f,  // 3
+        0x66,  // 4
+        0x6d,  // 5
+        0x7d,  // 6
+        0x07,  // 7
+        0x7f,  // 8
+        0x67   // 9
+};
+
+void SevenSegment_Update(uint8_t number){
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, ((number>>0)&0x01)); // a
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, ((number>>1)&0x01)); // b
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, ((number>>2)&0x01)); // c
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, ((number>>3)&0x01)); // d
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, ((number>>4)&0x01)); // e
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, ((number>>5)&0x01)); // f
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, ((number>>6)&0x01)); // g
+
+}
+
+
+void DisplayTxData(uint32_t value) {
+    // Extract individual digits
+    uint8_t temp1 = (value / 1000) % 10; // Thousands place
+    uint8_t temp2 = (value / 100) % 10;  // Hundreds place
+    uint8_t temp3 = (value / 10) % 10;   // Tens place
+    uint8_t temp4 = value % 10;          // Units place
+
+        // Display thousands digit
+        SevenSegment_Update(segmentNumber[temp1]);
+        D1_LOW();  // Activate D1
+        HAL_Delay(7); // Short delay for this digit
+        D1_HIGH(); // Deactivate D1
+
+
+        // Display hundreds digit
+        SevenSegment_Update(segmentNumber[temp2]);
+        D2_LOW();  // Activate D2
+        HAL_Delay(7); // Short delay for this digit
+        D2_HIGH(); // Deactivate D2
+
+        // Display tens digit
+        SevenSegment_Update(segmentNumber[temp3]);
+        D3_LOW();  // Activate D3
+        HAL_Delay(7); // Short delay for this digit
+        D3_HIGH(); // Deactivate D3
+
+        // Display units digit
+        SevenSegment_Update(segmentNumber[temp4]);
+        D4_LOW();  // Activate D4
+        HAL_Delay(7); // Short delay for this digit
+        D4_HIGH(); // Deactivate D4
+
+
+
+}
+
+
+
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -49,7 +49,6 @@ TIM_HandleTypeDef htim14;
 CAN_HandleTypeDef hcan1;
 CAN_TxHeaderTypeDef TxHeader;
 CAN_RxHeaderTypeDef RxHeader;
-//uint32_t TxMailbox1[4];
 uint32_t TxMailbox;
 uint8_t RxData[8];
 uint8_t TxData[8];
@@ -69,93 +68,6 @@ static void MX_TIM11_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
-// static variables keep scope within various function calls
-
-uint8_t temp1, temp2, temp3, temp4;
-
-#define D1_HIGH() HAL_GPIO_WritePin(D1_GPIO_Port, D1_Pin, GPIO_PIN_SET)
-#define D1_LOW() HAL_GPIO_WritePin(D1_GPIO_Port, D1_Pin, GPIO_PIN_RESET)
-#define D2_HIGH() HAL_GPIO_WritePin(D2_GPIO_Port, D2_Pin, GPIO_PIN_SET)
-#define D2_LOW() HAL_GPIO_WritePin(D2_GPIO_Port, D2_Pin, GPIO_PIN_RESET)
-#define D3_HIGH() HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, GPIO_PIN_SET)
-#define D3_LOW() HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, GPIO_PIN_RESET)
-#define D4_HIGH() HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, GPIO_PIN_SET)
-#define D4_LOW() HAL_GPIO_WritePin(D4_GPIO_Port, D4_Pin, GPIO_PIN_RESET)
-#define DP_ON() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET)  // Turn on decimal point
-#define DP_OFF() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET)  // Turn off decimal point
-
-uint32_t counter = 10; // Start at 10.5
-
-
-
-uint8_t segmentNumber[10] = {
-        0x3f,  // 0
-        0x06,  // 1
-        0x5b,  // 2
-        0x4f,  // 3
-        0x66,  // 4
-        0x6d,  // 5
-        0x7d,  // 6
-        0x07,  // 7
-        0x7f,  // 8
-        0x67   // 9
-};
-/*
- * Configure the Correct Digits For the 7-Segment
- *
- *  */
-void SevenSegment_Update(uint8_t number){
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, ((number>>0)&0x01)); // a
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, ((number>>1)&0x01)); // b
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, ((number>>2)&0x01)); // c
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, ((number>>3)&0x01)); // d
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, ((number>>4)&0x01)); // e
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, ((number>>5)&0x01)); // f
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, ((number>>6)&0x01)); // g
-
-}
-
-
-
-
-void DisplayTxData(uint32_t value) {
-    // Extract individual digits
-    uint8_t temp1 = (value / 1000) % 10; // Thousands place
-    uint8_t temp2 = (value / 100) % 10;  // Hundreds place
-    uint8_t temp3 = (value / 10) % 10;   // Tens place
-    uint8_t temp4 = value % 10;          // Units place
-
-        // Display thousands digit
-        SevenSegment_Update(segmentNumber[temp1]);
-        D1_LOW();  // Activate D1
-        HAL_Delay(7); // Short delay for this digit
-        D1_HIGH(); // Deactivate D1
-
-
-        // Display hundreds digit
-        SevenSegment_Update(segmentNumber[temp2]);
-        D2_LOW();  // Activate D2
-        HAL_Delay(7); // Short delay for this digit
-        D2_HIGH(); // Deactivate D2
-
-        // Display tens digit
-        SevenSegment_Update(segmentNumber[temp3]);
-        D3_LOW();  // Activate D3
-        HAL_Delay(7); // Short delay for this digit
-        D3_HIGH(); // Deactivate D3
-
-        // Display units digit
-        SevenSegment_Update(segmentNumber[temp4]);
-        D4_LOW();  // Activate D4
-        HAL_Delay(7); // Short delay for this digit
-        D4_HIGH(); // Deactivate D4
-
-
-    //}
-}
-
-
 
 
 
@@ -215,15 +127,6 @@ int main(void)
 	  TxData[0] = 0x23;
       TxData[1] = 0x44;
       TxData[2] = 0x69;
-
-//	  static uint32_t last_toggle_time = 0;
-//	          if (HAL_GetTick() - last_toggle_time >= 5000) { // 5000ms = 5 seconds
-//	        	  TxData[1] = 0x00;
-//	              last_toggle_time = HAL_GetTick(); // Update the last toggle time
-//	          }
-//	  	  // Test code to flash LED
-	  	  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	      //HAL_Delay(300); // Blink every 500 ms
 
 
 	 if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
@@ -320,7 +223,7 @@ static void MX_CAN1_Init(void)
 
 	hcan1.Instance = CAN1;
 	  hcan1.Init.Prescaler = 9;
-	  hcan1.Init.Mode = CAN_MODE_NORMAL;
+	  hcan1.Init.Mode = CAN_MODE_LOOPBACK;
 	  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
 	  hcan1.Init.TimeSeg1 = CAN_BS1_7TQ;
 	  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
@@ -351,43 +254,15 @@ static void MX_CAN1_Init(void)
 	  sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
 	  sFilterConfig.SlaveStartFilterBank = 14;
 
-	  if (HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig) != HAL_OK)
-	  {
-	    /* Filter configuration Error */
-	    Error_Handler();
-	  }
+	  HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig);
 
-	  /*##-3- Start the CAN peripheral ###########################################*/
-	  if (HAL_CAN_Start(&hcan1) != HAL_OK)
-	  {
-	    /* Start Error */
-	    Error_Handler();
-	  }
+	  HAL_CAN_Start(&hcan1);
 
-	  /*##-4- Activate CAN RX notification #######################################*/
-	  if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
-	  {
-	    /* Notification Error */
-	    Error_Handler();
-	  }
+	  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+
 
   /* USER CODE END CAN1_Init 1 */
-  hcan1.Instance = CAN1;
-  hcan1.Init.Prescaler = 9;
-  hcan1.Init.Mode = CAN_MODE_NORMAL;
-  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan1.Init.TimeSeg1 = CAN_BS1_7TQ;
-  hcan1.Init.TimeSeg2 = CAN_BS2_2TQ;
-  hcan1.Init.TimeTriggeredMode = DISABLE;
-  hcan1.Init.AutoBusOff = DISABLE;
-  hcan1.Init.AutoWakeUp = DISABLE;
-  hcan1.Init.AutoRetransmission = DISABLE;
-  hcan1.Init.ReceiveFifoLocked = DISABLE;
-  hcan1.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+
   /* USER CODE BEGIN CAN1_Init 2 */
 	        TxHeader.StdId = 0x0446;  // ID 2 (to match H7's filter)
 	    	TxHeader.ExtId = 0x01; // may need depending on the size of the bits being sent from ecu
